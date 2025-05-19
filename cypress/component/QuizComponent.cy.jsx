ca0fc7d0acc6f501be61b2bfba49e6848b4ecc68
Emily <eholt179@gmail.com>
2025-05-19 12:45:51 -0600

    updated file layout

--- cypress/component/QuizComponent.cy.jsx
+++ cypress/component/QuizComponent.cy.jsx
@@ -1,8 +1,16 @@
-import Quiz from '../../client/src/components/Quiz;
+// import React from 'react';
+import { mount } from 'cypress/react';
+import Quiz from '../../client/src/components/Quiz';
 
 describe('Quiz Component', () => {
-    it('renders quiz questions', () => {
-        cy.mount(<Quiz />)
-        cy.get('[data-cy="question"]').should('exist')
-    })
-})
+  it('renders the Start Quiz button', () => {
+    mount(<Quiz />);
+    cy.contains('Start Quiz').should('be.visible');
+  });
+
+  it('renders the first question after start', () => {
+    mount(<Quiz />);
+    cy.contains('Start Quiz').click();
+    cy.get('[data-cy="question"]').should('exist');
+  });
+});
